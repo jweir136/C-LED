233007911ba83b50b46b0242305e6f940426e1e0
Jacob Weir <jacobweir@Jacobs-MacBook-Pro.local>
2021-08-09 08:43:24 -0400

    add the LED on/off code

--- C-LED/main.cpp
+++ C-LED/main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include <wiringPi.h>
+
+#define WAITTIME    1000
+#define PIN         0
+
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
+    wiringPiSetup();
+
+    pinMode(PIN, OUTPUT);
+
+    for (int i = 0; i < 10; i++) {
+        digitalWrite(PIN, 1);
+        delay(WAITTIME);
+        digitalWrite(PIN, 0);
+        delay(WAITTIME);
+    }
 
     return (0);
 }
